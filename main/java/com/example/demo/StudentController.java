3d5ef1b927dfbd348e850b96e7f0f3a875e3d9a1
javasBC <98883189+javasBC@users.noreply.github.com>
2022-03-28 13:01:13 +0300

    Add files via upload

---
+++ main/java/com/example/demo/StudentController.java
@@ -0,0 +1,64 @@
+package com.example.demo;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+@RestController
+@RequestMapping("student")
+public class StudentController {
+    private static List<StudentDTO> students = new ArrayList<StudentDTO>();
+    private static int id = 0;
+
+    //static ctor
+    static {
+        students.add(new StudentDTO(getId(), 3, 88, "hodi", "java"));
+        students.add(new StudentDTO(getId(), 2, 90, "rami", "c#"));
+        students.add(new StudentDTO(getId(), 1, 99, "liora", "c++"));
+        students.add(new StudentDTO(getId(), 8, 65, "alex", "c"));
+        students.add(new StudentDTO(getId(), 4, 83, "bar", "js"));
+        students.add(new StudentDTO(getId(), 1, 100, "shiri", "html"));
+    }
+
+    public static int getId() {
+        return id++;
+    }
+
+    @GetMapping("/")
+    public List<StudentDTO> getStudents() {
+        return students;
+    }
+
+    @GetMapping("/{id}")
+    public StudentDTO getStudents(@PathVariable("id") int id) {
+        return students.stream().filter(s -> s.getId() == id).findFirst()
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+    }
+
+    @PostMapping("/")
+    public void addStudent(@RequestBody StudentDTO student) {
+        //if(student==null)
+        //    throw  new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        student.setId(getId());
+        students.add(student);
+    }
+
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable("id") int id) {
+        students.remove(getStudents(id));
+    }
+
+    @PutMapping("/{id}")
+    public void update(@RequestBody StudentDTO student, @PathVariable("id") int id) {
+        students.remove(id);
+        students.add(student);
+
+    }
+
+
+}
